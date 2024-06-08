6aa9ae576dff46557c9292098375fd2713aa6cf2
mediote <mediote90@gmail.com>
2024-06-08 15:50:13 -0300

    configure pytest

--- mlops_pipeline/storage.py
+++ mlops_pipeline/storage.py
@@ -9,31 +9,54 @@ class Storage:
             .getOrCreate()
 
     def obtem_estado_execucao_atual_pipeline(self, nome_modal: str, nome_projeto: str, nome_modelo: str, tipo_esteira: int) -> pd.DataFrame:
-        query = f"""(
-                    select 
-                        *
-                    from 
-                        hive_metastore.controle.tbl_controle_esteira_{tipo_esteira}
-                    where 
-                        data_criacao = (
-                            select
-                                max(data_criacao) 
-                            from 
-                                hive_metastore.controle.tbl_controle_esteira_{tipo_esteira}
-                            where
-                                nome_modal = '{nome_modal}' 
-                                and nome_projeto = '{nome_projeto}' 
-                                and nome_modelo = '{nome_modelo}' 
-                        )
-                )"""
+        """
+        Obtém o estado de execução atual do pipeline filtrando um DataFrame Parquet.
 
+        Args:
+            nome_modal (str): Nome do modal (e.g., rodovias, aeroportos).
+            nome_projeto (str): Nome do projeto.
+            nome_modelo (str): Nome do modelo.
+            tipo_esteira (int): Tipo da esteira.
+
+        Returns:
+            pd.DataFrame: DataFrame contendo o registro mais recente do estado de execução atual do pipeline.
+
+        Raises:
+            Exception: Se houver um erro ao acessar ou manipular o DataFrame.
+        """
         try:
-            df = self.spark.sql(query)
-            return df.toPandas()
+            # Carrega o DataFrame Parquet
+            df = self.spark.read.parquet(
+                f'hive_metastore/controle/tbl_controle_esteira_{tipo_esteira}.parquet')
+
+            # Converte para pandas DataFrame para facilitar a manipulação
+            df = df.toPandas()
+
+            # Filtra os dados conforme os critérios especificados
+            filtered_df = df[
+                (df['nome_modal'] == nome_modal) &
+                (df['nome_projeto'] == nome_projeto) &
+                (df['nome_modelo'] == nome_modelo)
+            ]
+
+            # Obtém a linha com a data_criacao mais recente
+            latest_record = filtered_df.loc[filtered_df['data_criacao'].idxmax()]
+
+            return latest_record.to_frame().T  # Converte de volta para DataFrame
         except Exception as e:
-            raise Exception(f"Error executing SQL query: {e}")
+            raise Exception(f"Error accessing DataFrame: {e}")
 
     def grava_estado_execucao_atual_pipeline(self, execucao_atual: pd.DataFrame, tipo_esteira: int):
+        """
+        Grava o estado de execução atual do pipeline em uma tabela Parquet.
+
+        Args:
+            execucao_atual (pd.DataFrame): DataFrame contendo o estado de execução atual do pipeline.
+            tipo_esteira (int): Tipo da esteira.
+
+        Raises:
+            Exception: Se houver um erro ao inserir os dados na tabela.
+        """
         try:
             sdf = self.spark.createDataFrame(execucao_atual)
             sdf.write.mode('append').insertInto(
--- mlops_pipeline/utils.py
+++ mlops_pipeline/utils.py
@@ -4,10 +4,22 @@ import pandas as pd
 
 
 def obtem_percentual_restante_validade_modelo(execucao_atual: pd.DataFrame) -> float:
+    """
+    Calcula o percentual restante de validade do modelo com base na data de validade e nos dias de validade.
+
+    Args:
+        execucao_atual (pd.DataFrame): DataFrame contendo os dados de execução atual do pipeline, incluindo
+            "data_validade_modelo" e "dias_validade_modelo".
+
+    Returns:
+        float: Percentual de dias restantes antes do modelo expirar. Se o número de dias de validade for zero,
+            retorna 0.
+    """
     data_validade_modelo = execucao_atual["data_validade_modelo"].iloc[0]
     dias_validade_modelo = execucao_atual["dias_validade_modelo"].iloc[0]
     dias_ate_fim_validade = (
-        datetime.strptime(data_validade_modelo, "%Y-%m-%d").date() - datetime.now().date()
+        datetime.strptime(data_validade_modelo, "%Y-%m-%d").date() -
+        datetime.now().date()
     ).days
     if dias_validade_modelo > 0:
         percentual_restante_validade_modelo = round(
@@ -15,5 +27,4 @@ def obtem_percentual_restante_validade_modelo(execucao_atual: pd.DataFrame) -> f
         )
     else:
         percentual_restante_validade_modelo = 0
-    return percentual_restante_validade_modelo
     return percentual_restante_validade_modelo
