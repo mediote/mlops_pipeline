efc5df9eace3c6245e16194147bfddb4d34e28bc
mediote <mediote90@gmail.com>
2024-06-08 17:14:45 -0300

    fix storage path

--- mlops_pipeline/main.py
+++ mlops_pipeline/main.py
@@ -6,7 +6,7 @@ from mlops_pipeline.storage import Storage
 
 class Pipeline:
     def __init__(self, base_path):
-        self.storage_path = Storage(base_path)
+        self.storage = Storage(base_path)
         self.saopaulo_timezone = pytz.timezone("America/Sao_Paulo")
 
     def inicializa_pipeline(self, params: dict) -> str:
