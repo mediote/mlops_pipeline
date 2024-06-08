5b926b255da265cc36cf55a0955a1840cd9d2074
mediote <mediote90@gmail.com>
2024-06-08 18:17:28 -0300

    fix storage path

--- mlops_pipeline/steps/inicializa_pipeline.py
+++ mlops_pipeline/steps/inicializa_pipeline.py
@@ -15,8 +15,13 @@ class InicializaPipelineParams(BaseModel):
     tipo_modelo: str
     tipo_esteira: int
     qtd_permitida_retreino: int
+    limiar_maximo_drift: float
     limiar_minino_acc: float
     dias_validade_modelo: int
+    qtd_dias_treino_inicial: int
+    qtd_dias_range_retreino_01: int
+    qtd_dias_range_retreino_02: int
+    qtd_dias_range_retreino_03: int
     email_usuario: str
     data_inicio_etapa_execucao_pipeline: datetime
 
@@ -34,13 +39,18 @@ def inicializa_pipeline(storage: Storage, delta_path: str, params: dict) -> str:
     tipo_esteira = validated_params.tipo_esteira
     qtd_permitida_retreino = validated_params.qtd_permitida_retreino
     limiar_minino_acc = validated_params.limiar_minino_acc
+    limiar_maximo_drift = validated_params.limiar_maximo_drift
     dias_validade_modelo = validated_params.dias_validade_modelo
+    qtd_dias_treino_inicial = validated_params.qtd_dias_treino_inicial
+    qtd_dias_range_retreino_01 = validated_params.qtd_dias_range_retreino_01
+    qtd_dias_range_retreino_02 = validated_params.qtd_dias_range_retreino_02
+    qtd_dias_range_retreino_03 = validated_params.qtd_dias_range_retreino_03
     email_usuario = validated_params.email_usuario
     data_inicio_etapa_execucao_pipeline = validated_params.data_inicio_etapa_execucao_pipeline
 
     saopaulo_timezone = pytz.timezone("America/Sao_Paulo")
     agora = datetime.now(saopaulo_timezone)
- 
+
     execucao_atual = storage.obtem_estado_execucao_atual_pipeline(
         delta_path, nome_modal, nome_projeto, nome_modelo)
 
@@ -109,14 +119,14 @@ def inicializa_pipeline(storage: Storage, delta_path: str, params: dict) -> str:
             "qtd_linhas_predicao": 0,
             "limiar_minino_acc": limiar_minino_acc,
             "valor_medido_acc": 0,
-            "qtd_dias_treino_inicial": 0,
-            "qtd_dias_range_retreino_01": 0,
-            "qtd_dias_range_retreino_02": 0,
-            "qtd_dias_range_retreino_03": 0,
+            "qtd_dias_treino_inicial": qtd_dias_treino_inicial,
+            "qtd_dias_range_retreino_01": qtd_dias_range_retreino_01,
+            "qtd_dias_range_retreino_02": qtd_dias_range_retreino_02,
+            "qtd_dias_range_retreino_03": qtd_dias_range_retreino_03,
             "etapa_retreino_modelo": 0,
             "qtd_permitida_retreino": qtd_permitida_retreino,
             "qtd_medida_retreino": 0,
-            "limiar_maximo_drift": 0,
+            "limiar_maximo_drift": limiar_maximo_drift,
             "valor_medido_drift": 0,
             "nome_cluster_execucao": "adb_dataops_ds_dev",
             "utilizacao_cpu": 0,
