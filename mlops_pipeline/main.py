04435ac895b8fff4239d14d43359c6cd2ec89d16
mediote <mediote90@gmail.com>
2024-06-08 16:29:48 -0300

    configure pytest

--- mlops_pipeline/main.py
+++ mlops_pipeline/main.py
@@ -1,8 +1,6 @@
 import pytz
 
 from mlops_pipeline.steps.inicializa_pipeline import inicializa_pipeline
-from mlops_pipeline.steps.monitora_e_prediz import monitora_drift_faz_predicoes
-from mlops_pipeline.steps.treina_avalia_modelos import treina_avalia_modelos
 from mlops_pipeline.storage import Storage
 
 
@@ -13,9 +11,3 @@ class Pipeline:
 
     def inicializa_pipeline(self, params: dict) -> str:
         return inicializa_pipeline(self.storage, params)
-
-    def treina_avalia_modelos(self, params: dict) -> str:
-        return treina_avalia_modelos(self.storage, params)
-
-    def monitora_drift_faz_predicoes(self, params: dict) -> str:
-        return monitora_drift_faz_predicoes(self.storage, params)
