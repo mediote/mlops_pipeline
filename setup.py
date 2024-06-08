22173cc7a8502d86cad6640102a9d3cc33d15121
mediote <mediote90@gmail.com>
2024-06-08 15:43:26 -0300

    configure pytest

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-
+        "pandas>=1.0.0",
+        "pydantic>=1.0",
+        "pyspark>=3.0.0",
+        "pyodbc>=4.0.0",
+        "pytest>=6.0.0",
+        "pytest-mock>=3.0.0"
     ],
     entry_points={
         "console_scripts": [
