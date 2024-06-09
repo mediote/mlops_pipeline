613a5a4c4f9fb1ef42fb2a5ecef523ce809b4005
mediote <mediote90@gmail.com>
2024-06-09 13:51:11 -0300

    update control table schema

--- mlops_pipeline/steps.py
+++ mlops_pipeline/steps.py
@@ -19,10 +19,10 @@ class InitPipelineParams(BaseModel):
     limiar_maximo_drift: float
     limiar_minino_acc: float
     dias_validade_modelo: int
-    qtd_dias_treino_inicial: int
-    qtd_dias_range_retreino_01: int
-    qtd_dias_range_retreino_02: int
-    qtd_dias_range_retreino_03: int
+    qtd_dados_treinamento: int
+    qtd_dados_retreino_01: int
+    qtd_dados_retreino_02: int
+    qtd_dados_retreino_03: int
     email_usuario: str
     data_inicio_etapa_execucao_pipeline: datetime
 
@@ -43,10 +43,10 @@ def init_pipeline(params: dict, delta_path: str) -> str:
             - limiar_maximo_drift (float): Limiar máximo de drift.
             - limiar_minino_acc (float): Limiar mínimo de acurácia.
             - dias_validade_modelo (int): Dias de validade do modelo.
-            - qtd_dias_treino_inicial (int): Quantidade de dias de treino inicial.
-            - qtd_dias_range_retreino_01 (int): Quantidade de dias para o primeiro range de retreino.
-            - qtd_dias_range_retreino_02 (int): Quantidade de dias para o segundo range de retreino.
-            - qtd_dias_range_retreino_03 (int): Quantidade de dias para o terceiro range de retreino.
+            - qtd_dados_treinamento (int): Quantidade de dias de treino inicial.
+            - qtd_dados_retreino_01 (int): Quantidade de dias para o primeiro range de retreino.
+            - qtd_dados_retreino_02 (int): Quantidade de dias para o segundo range de retreino.
+            - qtd_dados_retreino_03 (int): Quantidade de dias para o terceiro range de retreino.
             - email_usuario (str): Email do usuário.
             - data_inicio_etapa_execucao_pipeline (datetime): Data de início da etapa de execução do pipeline.
 
@@ -70,10 +70,10 @@ def init_pipeline(params: dict, delta_path: str) -> str:
     limiar_minino_acc = validated_params.limiar_minino_acc
     limiar_maximo_drift = validated_params.limiar_maximo_drift
     dias_validade_modelo = validated_params.dias_validade_modelo
-    qtd_dias_treino_inicial = validated_params.qtd_dias_treino_inicial
-    qtd_dias_range_retreino_01 = validated_params.qtd_dias_range_retreino_01
-    qtd_dias_range_retreino_02 = validated_params.qtd_dias_range_retreino_02
-    qtd_dias_range_retreino_03 = validated_params.qtd_dias_range_retreino_03
+    qtd_dados_treinamento = validated_params.qtd_dados_treinamento
+    qtd_dados_retreino_01 = validated_params.qtd_dados_retreino_01
+    qtd_dados_retreino_02 = validated_params.qtd_dados_retreino_02
+    qtd_dados_retreino_03 = validated_params.qtd_dados_retreino_03
     email_usuario = validated_params.email_usuario
     data_inicio_etapa_execucao_pipeline = validated_params.data_inicio_etapa_execucao_pipeline
 
@@ -89,11 +89,11 @@ def init_pipeline(params: dict, delta_path: str) -> str:
         data_validade_modelo = run_state["data_validade_modelo"].iloc[0]
 
         if datetime.strptime(data_validade_modelo, "%Y-%m-%d").date() > now.date() and run_state["qtd_medida_retreino"].iloc[0] <= qtd_permitida_retreino:
-            if run_state["status_execucao_pipeline"].iloc[0] == "green":
+            if run_state["status_execucao_pipeline"].iloc[0] in ["white", "green"]:
                 status_execucao_pipeline = run_state["status_execucao_pipeline"].iloc[0]
-                run_state["resumo_execucao"] = "Preparando para drift/predicao"
-                run_state["id_execucao_pipeline"] = run_state["id_execucao_pipeline"].iloc[0] + 1
-                run_state["id_etapa_execucao_pipeline"] = 0
+                run_state["resumo_execucao_etapa"] = "Preparando para drift/predicao"
+                run_state["id_pipeline"] = run_state["id_pipeline"].iloc[0] + 1
+                run_state["id_etapa_pipeline"] = 0
                 run_state["data_inicio_etapa_execucao_pipeline"] = data_inicio_etapa_execucao_pipeline
                 run_state["data_fim_etapa_execucao_pipeline"] = datetime.now(
                     saopaulo_timezone)
@@ -102,9 +102,9 @@ def init_pipeline(params: dict, delta_path: str) -> str:
             else:
                 return "red"
         elif run_state["qtd_medida_retreino"].iloc[0] >= qtd_permitida_retreino:
-            run_state["id_execucao_pipeline"] = run_state["id_execucao_pipeline"].iloc[0] + 1
-            run_state["id_etapa_execucao_pipeline"] = 0
-            run_state["resumo_execucao"] = "Limite de Retreino Por Drift Excedido"
+            run_state["id_pipeline"] = run_state["id_pipeline"].iloc[0] + 1
+            run_state["id_etapa_pipeline"] = 0
+            run_state["resumo_execucao_etapa"] = "Limite de Retreino Por Drift Excedido"
             run_state["status_execucao_pipeline"] = "red"
             run_state["data_inicio_etapa_execucao_pipeline"] = data_inicio_etapa_execucao_pipeline
             run_state["data_fim_etapa_execucao_pipeline"] = datetime.now(
@@ -112,11 +112,11 @@ def init_pipeline(params: dict, delta_path: str) -> str:
             set_pipeline_run_state(run_state, delta_path)
             return "red"
         else:
-            run_state["id_execucao_pipeline"] = run_state["id_execucao_pipeline"].iloc[0] + 1
-            run_state["resumo_execucao"] = "Retreino por Validade"
+            run_state["id_pipeline"] = run_state["id_pipeline"].iloc[0] + 1
+            run_state["resumo_execucao_etapa"] = "Retreino por Validade"
             run_state["status_execucao_pipeline"] = "yellow"
-            run_state["etapa_retreino_modelo"] = run_state["etapa_retreino_modelo"].iloc[0] + 1
-            run_state["id_etapa_execucao_pipeline"] = 0
+            run_state["passo_etapa_retreino_modelo"] = run_state["passo_etapa_retreino_modelo"].iloc[0] + 1
+            run_state["id_etapa_pipeline"] = 0
             run_state["valor_medido_metrica_modelo"] = 0
             run_state["valor_medido_drift"] = 0
             run_state["data_inicio_etapa_execucao_pipeline"] = data_inicio_etapa_execucao_pipeline
@@ -126,16 +126,15 @@ def init_pipeline(params: dict, delta_path: str) -> str:
             return "white"
     else:
         run_state = pd.DataFrame([{
-            "id_experimento": 0,
             "nome_modal": nome_modal,
             "nome_projeto": nome_projeto,
-            "id_execucao_pipeline": 0,
-            "id_etapa_execucao_pipeline": 0,
+            "id_pipeline": 0,
+            "id_etapa_pipeline": 0,
             "status_execucao_pipeline": "white",
-            "etapa_execucao_pipeline": "Inicializa Pipeline",
+            "nome_etapa_pipeline": "Inicializa Pipeline",
             "data_inicio_etapa_execucao_pipeline": data_inicio_etapa_execucao_pipeline,
             "data_fim_etapa_execucao_pipeline": datetime.now(saopaulo_timezone),
-            "resumo_execucao": "Preparacao para Treinamento Inicial",
+            "resumo_execucao_etapa": "Preparacao para Treinamento Inicial",
             "nome_modelo": nome_modelo,
             "versao_modelo": "0.0",
             "tipo_modelo": tipo_modelo,
@@ -148,11 +147,11 @@ def init_pipeline(params: dict, delta_path: str) -> str:
             "qtd_linhas_predicao": 0,
             "limiar_minino_acc": limiar_minino_acc,
             "valor_medido_acc": .0,
-            "qtd_dias_treino_inicial": qtd_dias_treino_inicial,
-            "qtd_dias_range_retreino_01": qtd_dias_range_retreino_01,
-            "qtd_dias_range_retreino_02": qtd_dias_range_retreino_02,
-            "qtd_dias_range_retreino_03": qtd_dias_range_retreino_03,
-            "etapa_retreino_modelo": 0,
+            "qtd_dados_treinamento": qtd_dados_treinamento,
+            "qtd_dados_retreino_01": qtd_dados_retreino_01,
+            "qtd_dados_retreino_02": qtd_dados_retreino_02,
+            "qtd_dados_retreino_03": qtd_dados_retreino_03,
+            "passo_etapa_retreino_modelo": 0,
             "qtd_permitida_retreino": qtd_permitida_retreino,
             "qtd_medida_retreino": 0,
             "limiar_maximo_drift": limiar_maximo_drift,
@@ -173,7 +172,7 @@ class ExecutionStepParams(BaseModel):
     nome_modal: str
     nome_projeto: str
     nome_modelo: str
-    etapa_execucao_pipeline: str
+    nome_etapa_pipeline: str
     data_inicio_etapa_execucao_pipeline: datetime
 
 
@@ -199,15 +198,15 @@ def update_pipeline_execution_step(params: dict, delta_path: str) -> str:
     nome_projeto = validated_params.nome_projeto
     nome_modelo = validated_params.nome_modelo
     data_inicio_etapa_execucao_pipeline = validated_params.data_inicio_etapa_execucao_pipeline
-    etapa_execucao_pipeline = validated_params.etapa_execucao_pipeline
+    nome_etapa_pipeline = validated_params.nome_etapa_pipeline
 
     saopaulo_timezone = pytz.timezone("America/Sao_Paulo")
 
     run_state = get_pipeline_run_state(
         nome_modal, nome_projeto, nome_modelo, delta_path)
 
-    run_state["id_etapa_execucao_pipeline"] = run_state["id_etapa_execucao_pipeline"].iloc[0] + 1
-    run_state["etapa_execucao_pipeline"] = etapa_execucao_pipeline
+    run_state["id_etapa_pipeline"] = run_state["id_etapa_pipeline"].iloc[0] + 1
+    run_state["nome_etapa_pipeline"] = nome_etapa_pipeline
     run_state["data_inicio_etapa_execucao_pipeline"] = data_inicio_etapa_execucao_pipeline
     run_state["data_fim_etapa_execucao_pipeline"] = datetime.now(saopaulo_timezone)
 
